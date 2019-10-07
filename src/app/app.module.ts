c4f8f31738556092c2bf2a825d1f047ae27254ce
Teo Kai Xiang <dennis_teo@dyn-118-138-35-78.its.monash.edu.au>
2019-10-07 17:46:41 +1100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { ActorComponent } from "./actor/actor.component";
+import { DatabaseService } from "./database.service";
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
+import { NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, ActorComponent],
+  imports: [BrowserModule, HttpClientModule, FormsModule,NgbModule],
+  providers: [DatabaseService],
+  bootstrap: [AppComponent, ActorComponent],
 })
-export class AppModule { }
+export class AppModule {}
